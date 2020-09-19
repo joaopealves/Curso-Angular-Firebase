86e53178aae114217441e55a831d578f99319c77
João Pedro Alves <jotalmeida007@hotmail.com>
2020-09-19 10:39:41 -0300

    Adicionar componentes no html e bootstrap

--- aula/src/app/pagina1/pagina1.component.ts
+++ aula/src/app/pagina1/pagina1.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pagina1',
+  selector: 'pagina1',
   templateUrl: './pagina1.component.html',
-  styleUrls: ['./pagina1.component.css']
+  styleUrls: ['./pagina1.component.css'],
 })
 export class Pagina1Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
