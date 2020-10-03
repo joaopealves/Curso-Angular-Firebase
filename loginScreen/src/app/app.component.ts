a1d03c0f1d94c09e70e16e1ff3c577e39e3d7abe
João Pedro Alves <jotalmeida007@hotmail.com>
2020-10-03 10:04:25 -0300

    Criar novo projeto Angular

---
+++ loginScreen/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'loginScreen';
+  text = 'Bora lá de Angular';
+}
